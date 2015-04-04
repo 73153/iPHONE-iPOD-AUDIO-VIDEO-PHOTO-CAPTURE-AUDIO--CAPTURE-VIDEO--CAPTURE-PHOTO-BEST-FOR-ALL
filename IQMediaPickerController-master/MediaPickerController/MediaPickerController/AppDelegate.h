7a9e5d106ebb5fe4d3cb83e595e8fa6eb60b362c
victor <vivek2061990@gmail.com>
2015-04-04 19:07:43 +0530

    My first comit

---
+++ IQMediaPickerController-master/MediaPickerController/MediaPickerController/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  IQMediaPickerController
+//
+//  Copyright (c) 2013-14 Iftekhar Qurashi.
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
